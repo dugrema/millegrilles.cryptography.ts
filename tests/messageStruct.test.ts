74334e6ce56f8b548362a109ae70b968f37b7d5d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-07-21 16:55:35 -0400

    Commande signature test

--- tests/messageStruct.test.ts
+++ tests/messageStruct.test.ts
@@ -15,3 +15,17 @@ test('sign-message 1', async () => {
     let result = await message.verify();
     expect(result).toBe(true);
 });
+
+test('sign-command', async () => {
+    let signingKey = await newMessageSigningKey(PRIVATE_KEY);
+    let message = new MilleGrillesMessage(1721592075, 2, 'DUMMY CONTENT');
+    message.routage = {domaine: 'DUMMY-DOMAINE', action: 'DUMMY-ACTION'}
+    await message.sign(signingKey);
+    console.debug("Message ", message);
+    expect(message.pubkey).toStrictEqual('4ff87c18844b0575b77946fb84a469b58d931622672b3c2614439bc9b609cdc4');
+    expect(message.id).toStrictEqual('1b70b5cf9a7dc222aeb430864f930fc0fe448b6190b03b8739bf1387f56d3c72');
+    expect(Buffer.from(message.signature).length).toBe(128);
+
+    let result = await message.verify();
+    expect(result).toBe(true);
+});
