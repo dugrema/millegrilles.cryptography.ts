6ee526910ff14f2f288ddd7ed6f7466ee23397bd
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-07-23 07:14:31 -0400

    Added rollup for packaging

---
+++ lib/index.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+
+export * as certificates from "./certificates";
+export * as digest from "./digest";
+export * as ed25519 from "./ed25519";
+export * as encryptionMgs4 from "./encryption.mgs4";
+export * as encryption from "./encryption";
+export * as keymaster from "./keymaster";
+export * as messageStruct from "./messageStruct";
+export * as multiencoding from "./multiencoding";
+export * as random from "./random";
+export * as x25519 from "./x25519";
