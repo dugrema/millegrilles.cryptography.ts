c10c2eb2efbc10c5b67151bcc69d0d75da096343
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-12-14 18:22:44 -0500

    Removed dependencies on node-forge (custom branch ed25519).

--- lib/index.ts
+++ lib/index.ts
@@ -10,5 +10,3 @@ export * as messageStruct from "./messageStruct";
 export * as multiencoding from "./multiencoding";
 export * as random from "./random";
 export * as x25519 from "./x25519";
-export * as forgeCsr from './forgeCsr';
-export * as forgePrivateKey from './forgePrivateKey';
